af5f4f8bc9b9888662502f22818d6f100316eefc
Ivan Kar <i.kar.kh@gmail.com>
2018-11-09 13:44:36 +0200

    Initial commit

---
+++ precache-manifest.8efd116a589b8cd17870a876ba691c45.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42fd19eb9586e7047ba3",
+    "url": "/static/js/main.42fd19eb.chunk.js"
+  },
+  {
+    "revision": "3d2c4ec3282626af3d3b",
+    "url": "/static/js/1.3d2c4ec3.chunk.js"
+  },
+  {
+    "revision": "3d2c4ec3282626af3d3b",
+    "url": "/static/css/1.177d59ce.chunk.css"
+  },
+  {
+    "revision": "3d6185ec6905ce6cdbc84f0f605ba475",
+    "url": "/index.html"
+  }
+];
